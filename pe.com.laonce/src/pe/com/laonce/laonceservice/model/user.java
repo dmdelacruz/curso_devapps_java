24813b46faabb1503518dcdc70ad7b5876c7d114
Rommel Aaron Melgar Lopez <rmelgarl@usersad.everis.int>
2017-07-15 04:53:58 -0500

    Nuevo

--- pe.com.laonce/src/pe/com/laonce/laonceservice/model/user.java
+++ pe.com.laonce/src/pe/com/laonce/laonceservice/model/user.java
@@ -4,4 +4,10 @@ package pe.com.laonce.laonceservice.model;
  * Created by rmelgarl on 15/07/2017.
  */
 public class user {
+    private int id;
+    private String password;
+    private String email;
+    private usertipe id;
+
+
 }
